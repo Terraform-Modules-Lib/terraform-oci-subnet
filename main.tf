3914a1567874e1d8c211338ca9b061606c1ae603
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-24 12:09:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,45 +14,22 @@ locals {
   vcn_id = var.vcn_id
   cidr = var.cidr
   name = var.name
+  name = var.public
   
-  vcn = data.oci_core_vcn.this
-  subnet = oci_core_subnet.this
-  routing_table = oci_core_route_table.this
+  vcn = data.oci_core_vcn.vcn
+  subnet = oci_core_subnet.subnet
 }
 
-data "oci_core_vcn" "this" {
+data "oci_core_vcn" "vcn" {
   vcn_id = local.vcn_id
 }
 
-resource "oci_core_subnet" "this" {
+resource "oci_core_subnet" "subnet" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
-  prohibit_public_ip_on_vnic = false
+  prohibit_public_ip_on_vnic = local.public
 
   cidr_block = local.cidr
-  route_table_id = local.routing_table.id
   display_name = local.name
   dns_label = local.name
-}
-
-resource "oci_core_internet_gateway" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-  
-  display_name = local.name
-}
-
-resource "oci_core_route_table" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-
-  display_name = local.name
-  
-  route_rules = [{
-    network_entity_id = oci_core_internet_gateway.this.id
-    
-    description = format("Internet route for %s subnet", local.name)
-    destination_type = "CIDR_BLOCK"
-    destination = "0.0.0.0/0"
-  }]
 }
