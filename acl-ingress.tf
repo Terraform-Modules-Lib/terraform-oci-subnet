fb5c61f6afa9809467916bc4ce36db325ae14530
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 14:43:41 +0200

    Update acl-ingress.tf

--- acl-ingress.tf
+++ acl-ingress.tf
@@ -15,7 +15,7 @@ resource "oci_core_security_list" "ingress" {
       protocol = local.protocol_map[ingress_security_rules.value.protocol]
       
       dynamic "tcp_options" {
-        for_each = ingress_security_rules.value.protocol == "tcp" ? { "${ingress_security_rules.key}" = ingress_security_rules.value } : {}
+        for_each = ingress_security_rules.value.protocol == "tcp" ? zipmap([ingress_security_rules.key], [ingress_security_rules.value]) : {}
         
         content {
           min = tcp_options.value.dst_port_min
@@ -28,24 +28,24 @@ resource "oci_core_security_list" "ingress" {
       }
       
       dynamic "udp_options" {
-        for_each = ingress_security_rules.value.protocol == "udp" ? { "${ingress_security_rules.key}" = ingress_security_rules.value } : {}
+        for_each = ingress_security_rules.value.protocol == "udp" ? zipmap([ingress_security_rules.key], [ingress_security_rules.value]) : {}
         
         content {
-          min = udp_options.value.dst_min
-          max = udp_options.value.dst_max
+          min = udp_options.value.dst_port_min
+          max = udp_options.value.dst_port_max
           source_port_range {
-            min = udp_options.value.src_min
-            max = udp_options.value.src_max
+            min = udp_options.value.src_port_min
+            max = udp_options.value.src_port_max
           }
         }
       }
       
       dynamic "icmp_options" {
-        for_each = ingress_security_rules.value.protocol == "icmp" ? { "${ingress_security_rules.key}" = ingress_security_rules.value } : {}
+        for_each = ingress_security_rules.value.protocol == "icmp" ? zipmap([ingress_security_rules.key], [ingress_security_rules.value]) : {}
         
         content {
           type = icmp_options.value.type
-          code = icmp_options.value.type
+          code = icmp_options.value.code
         }
       }
     }
